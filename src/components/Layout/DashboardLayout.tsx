0b1dc5922560bcec72a6ec60c3f16eeb98b6442e
Bruno Andrade Dias <andradedias.bruno@hotmail.com>
2025-07-04 13:37:03 -0300

    init

---
+++ src/components/Layout/DashboardLayout.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "../Sidebar";
+import Header from "../Header";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function DashboardLayout({ children }: Props) {
+  return (
+    <div className="flex h-screen bg-[#f4f7fe]">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
